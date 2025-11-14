6b381591bae6c47cd64cd41a21c41edbd8e3e2a0
Kris Brooks <kbrooks8193@gmail.com>
2025-11-14 17:51:04 -0500

    Added nativewind css files.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,16 +4,12 @@ import { View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center"
     >
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Link href="./properties/1">Property</Link>
     </View>
   );
 }
