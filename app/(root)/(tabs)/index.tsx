749709092dc480955b3b64f7031b83fe9512daaa
Kris Brooks <kbrooks8193@gmail.com>
2025-11-14 20:09:07 -0500

    updated app setting configs to use new assets.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       className="flex-1 items-center justify-center"
     >
+      <Text className="text-xl font-rubik-medium">My Name is Kris!</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
